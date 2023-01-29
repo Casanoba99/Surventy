1521432a60fb278cdf47d31320695b0601b43493
Casanoba99 <alejandro.canto.99@gmail.com>
2023-01-29 16:23:42 +0100

    Cositas

--- Assets/Scripts/Enemigos/EnemigosInput.cs
+++ Assets/Scripts/Enemigos/EnemigosInput.cs
@@ -6,26 +6,39 @@ using UnityEngine.Rendering.Universal;
 
 public class EnemigosInput : MonoBehaviour
 {
-    Coroutine velCoro;
+    Coroutine dañoCoro, atacarCoro;
     GameManager Manager => GameManager.gm;
-    Animator anims => GetComponent<Animator>();
+    Animator Anims => GetComponent<Animator>();
     AudioSource Source => GetComponent<AudioSource>();
     ParticleSystem Ps => GetComponent<ParticleSystem>();
 
     bool vivo = true;
+    Vector3 destino;
 
     public Transform target;
+    [Range(0, 1)]
+    public float distDaño = .4f;
+
     [Header("Stats")]
     public int vida;
+    public bool velocista;
     public float velocidad;
 
     void Update()
     {
         if (Manager.start)
-            transform.position = Vector3.MoveTowards(transform.position, target.position, velocidad * Manager.tiempoDelta);
+        {
+            if (!velocista)
+                transform.position = Vector3.MoveTowards(transform.position, target.position, velocidad * Manager.tiempoDelta);
+            else
+            {
+                Start_Arremeter();
+                transform.position = Vector3.MoveTowards(transform.position, destino, velocidad * Manager.tiempoDelta);
+            }
 
-        if (Vector3.Distance(target.position, transform.position) < .4f && vida > 0)
-            target.GetComponent<PlayerInput>().Start_PierdeVida();
+            if (Vector3.Distance(target.position, transform.position) < distDaño && vida > 0)
+                target.GetComponent<PlayerInput>().Start_PierdeVida();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -47,15 +60,14 @@ public class EnemigosInput : MonoBehaviour
         }
     }
 
-
     void Start_RecibirDaño(GameObject obj)
     {
-        velCoro ??= StartCoroutine(RecibirDaño(obj));
+        dañoCoro ??= StartCoroutine(RecibirDaño(obj));
     }
 
     IEnumerator RecibirDaño(GameObject obj)
     {
-        anims.SetTrigger("Daño");
+        Anims.SetTrigger("Daño");
         Source.Play();
         PerderVida(obj);
         float vel = velocidad;
@@ -63,7 +75,7 @@ public class EnemigosInput : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         velocidad = vel;
 
-        velCoro = null;
+        dañoCoro = null;
     }
     void PerderVida(GameObject obj)
     {
@@ -87,5 +99,45 @@ public class EnemigosInput : MonoBehaviour
         {
             vida -= obj.GetComponent<BotTurretShoot>().daño;
         }
+    }
+
+    void Start_Arremeter()
+    {
+        atacarCoro ??= StartCoroutine(ArremeterDestino());
+    }
+
+    IEnumerator ArremeterDestino()
+    {
+        Vector2 tPos = target.position;
+
+        tPos += new Vector2(Mathf.Sign(tPos.x), Mathf.Sign(tPos.y));
+        Debug.Log(tPos + " + " + new Vector2(Mathf.Sign(tPos.x), Mathf.Sign(tPos.y)));
+
+        //if (tPos == Vector2.positiveInfinity)
+        //{
+        //    tPos += Vector2.one;
+        //    Debug.Log(tPos + " + " + Vector2.one);
+
+        //}
+        //else if (tPos == Vector2.negativeInfinity)
+        //{
+        //    tPos -= Vector2.one;
+        //    Debug.Log(tPos + " - " + Vector2.one);
+        //}
+        //else if (tPos == new Vector2(float.PositiveInfinity, float.NegativeInfinity))
+        //{
+        //    tPos += new Vector2(1, -1);
+        //    Debug.Log(tPos + " + " + new Vector2(1, -1));
+        //}
+        //else if (tPos == new Vector2(float.NegativeInfinity, float.PositiveInfinity))
+        //{
+        //    tPos += new Vector2(-1, 1);
+        //    Debug.Log(tPos + " + " + new Vector2(-1, 1));
+        //}
+
+        Debug.Log("Resultado " + tPos);
+        destino = tPos;
+        yield return new WaitForSeconds(3f);
+        atacarCoro = null;
     }
 }
